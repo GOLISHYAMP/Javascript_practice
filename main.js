e0fd12adf2cc34d8886a16b376fd1bc95e85ac05
Shyam Goli <golishyamgolisai@gmail.com>
2023-08-06 00:09:05 +0530

    Crash course completed on JS

--- main.js
+++ main.js
@@ -108,6 +108,31 @@
 // });
 
 
+const myform = document.querySelector("#my-form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const msg = document.querySelector(".msg");
+const userlist = document.querySelector("#users");
+
+myform.addEventListener("submit" , onSubmit);
+
+function onSubmit(e){
+    e.preventDefault();
+    if (nameInput.value === "" || emailInput.value === ""){
+        msg.classList.add("error");
+        msg.innerHTML = "please enter all fields!";
+        setTimeout( () => msg.remove(), 3000);
+    }
+    else{
+        const li = document.createElement('li')
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+        userlist.appendChild(li);
+        // userlist.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+        nameInput.value = "";
+        emailInput.value = "";
+    }
+}
+
 
 
 
