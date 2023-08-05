371ab9d464a3b2c79e0472c08fa7061c3955479f
Shyam Goli <golishyamgolisai@gmail.com>
2023-08-05 20:52:02 +0530

    interesting things DOM learned

--- main.js
+++ main.js
@@ -20,3 +20,79 @@
 //     return N1+N2;
 // }
 // console.log(add(4,2));
+
+
+// Constructor function
+// function Person (firstname, lastname, dob){
+//     this.firstname = firstname;
+//     this.lastname = lastname;
+//     this.dob = new Date(dob);
+//     this.getfullname = () => {
+//         return (`${this.firstname} ${this.lastname}`);
+//     }
+    
+// }
+// Person.prototype.getdob = () => {
+//     return this.dob;
+// }
+
+// //Object creation
+// const person1 = new Person("shyam", "goli", '31-05-2000')
+// const person2 = new Person("prashant", "vasala", '31-08-2000')
+
+// console.log(person1.firstname)
+// // console.log(person1.dob.getDate())
+// console.log(person1.getfullname())
+// console.log(person1.getdob())
+// console.log(person2.firstname)
+// console.log(person2.getfullname())
+
+
+// class Person{
+//     constructor(firstname, lastname,dob){
+//         this.firstname = firstname;
+//         this.lastname = lastname;
+//         this.dob = dob;
+//     }
+
+//     getUppercaseName(){
+//         return this.firstname.toUpperCase();
+//     }
+// }
+
+// let person1 = new Person("shyam", "goli", "31-05-2000")
+// console.log(person1.dob);
+// console.log(person1.getUppercaseName());
+
+
+//***********DOMS concept  **************
+// Single selectors
+// console.log(document.getElementById("my-form"));
+// console.log(document.querySelector(".item"));
+
+// console.log(document.querySelectorAll(".item"));
+// console.log(document.getElementsByClassName("item"));
+// console.log(document.getElementsByTagName("li"));
+
+// const items = document.querySelectorAll(".item");
+// items.forEach((item) => console.log(item));
+
+// const items = document.querySelectorAll('.item');
+// items.forEach((item) => console.log(item));
+
+
+
+////////////////////////////////////////
+//////////// Manipulating DOM ///////////////
+//// Very interesting  ///////////////
+const ul = document.querySelector(".items")
+// ul.remove()
+// ul.lastElementChild.remove()
+
+ul.firstElementChild.textContent = "Hello"
+ul.children[1].textContent = "Shyam"
+ul.lastElementChild.innerHTML = "<h1>Ram</h1>"
+
+const btn = document.querySelector('.btn');
+btn.style.background = "red"
+btn.style.color = "black"
