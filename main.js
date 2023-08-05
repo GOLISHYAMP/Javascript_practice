6b92ee7234184d3cd48aa036c115b97efa44b187
Shyam Goli <golishyamgolisai@gmail.com>
2023-08-05 14:29:12 +0530

    changes made in main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+// const person = {
+//     name : "shyam",
+//     last : "goli",
+//     address : {
+//         city : "mumbai",
+//         pincode : 400018
+//     }
+// }
+// person.email = "shyam@gmail.com"
+// person.name = "rocky"
+// console.log(person)
+
+// function add(N1,N2){
+//     return N1+N2;
+// }
+// console.log(add(4,2));
+
+//Arror function
+// const add = (N1,N2) => {
+//     return N1+N2;
+// }
+// console.log(add(4,2));
